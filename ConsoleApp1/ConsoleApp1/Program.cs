59e6bf8a181a8f9aab1fc9ece5eb64238aa12bd1
chinthaka20010226 <prabathchinthaka361@gmail.com>
2023-11-07 14:19:12 +0530

    basis of C#

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,399 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("This is a same line...");
+            //Console.WriteLine("This is a new line...");
+            //Console.WriteLine('A');
+            //Console.WriteLine(1+3);
+            //Console.WriteLine(5-2);
+            //Console.WriteLine(3*2);
+            //Console.WriteLine(3/2);
+            //Console.WriteLine(5%2);
+
+
+
+            /*
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Clear();
+            Console.WriteLine("Chage the Console...");
+            */
+
+
+
+            //Console.Write("What is your name ? ");
+            //string name = Console.ReadLine();
+            //Console.WriteLine(name);
+            //Console.WriteLine($"Hello {name}");
+
+
+            /*
+            byte x = 7;
+            short y = 33;
+            int num = 11;
+            long num__ = 123L;
+
+            float num___ = 1.23F;
+            double num_ = 1.23D;
+
+            char ch = 'A';
+
+            string name = "chinthaka";
+
+            bool found = true;
+            */
+
+
+
+
+            /*
+            const int num = 15;
+            //num = 20;
+            Console.WriteLine(num);
+            */
+            /*
+            const int num_;
+            num_ = 77;
+            Console.WriteLine(num_);
+            */
+
+
+
+            /*
+             string name = "chinthaka";
+             Console.WriteLine("Hello "+name);
+             Console.WriteLine($"Hello {name}");
+            */
+
+
+
+            //Console.WriteLine("Biggest Integer : {0}", int.MaxValue);
+            //Console.WriteLine("Lowest Integer : {0}", int.MinValue);
+
+            //Console.WriteLine("Biggest Long : {0}", long.MaxValue);
+            //Console.WriteLine("Lowest Long : {0}", long.MinValue);
+
+            //Console.WriteLine("Biggest Float : {0}", float.MaxValue);
+            //Console.WriteLine("Lowest Float : {0}", float.MinValue);
+
+            //Console.WriteLine("Biggest Double : {0}", double.MaxValue);
+            //Console.WriteLine("Lowest Double : {0}", double.MinValue);
+
+            //decimal num = 3.143123456728937748843M;
+            //decimal num_ = 3.000000000000000000000M;
+            //Console.WriteLine("Dec : num + nmu_ = {0}", num+num_);
+
+            //Console.WriteLine("Biggest Decimal : {0}", decimal.MaxValue);
+            //Console.WriteLine("Lowest Decimal : {0}", decimal.MinValue);
+
+
+
+            /*
+            char ch = 'A';
+
+            int x = ch;
+            Console.WriteLine(x);
+
+            long y = x;
+            Console.WriteLine(y);
+
+            float z = y;
+            Console.WriteLine(z);
+
+            double d = z;
+            Console.WriteLine(d);
+            */
+
+            /*
+            double d = 1.23D;
+
+            float f = (float)d;
+            Console.WriteLine(f);
+
+            long l = (long)f;
+            Console.WriteLine(l);
+
+            int i = (int)l;
+            Console.WriteLine(i);
+
+            short s = (short)i;
+            Console.WriteLine(s);
+
+            byte b = (byte)s;
+            Console.WriteLine(b);
+           
+            decimal num = 1.23456M;
+
+            int i = (int)num;
+            Console.WriteLine(i);
+
+            double d = (double)num;
+            Console.WriteLine(d);
+            */
+
+
+
+
+
+            //Console.WriteLine("What is your name ? ");
+            //String name = Console.ReadLine();
+            //Console.WriteLine($"Hello {name}");
+            //Console.WriteLine("What's up "+name);
+
+
+            //Console.WriteLine("Input your age : ");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine($"My name is {name}"+"\n"+$"I am {age} years old");
+
+
+
+
+
+            //Console.WriteLine(Math.Max(3,77));
+            //int maxValue = Math.Max(3,77);
+            //Console.WriteLine(maxValue);
+
+            //int minValue = Math.Min(333,-1);
+            //double abs = Math.Abs(-1.23);
+            //int round = Convert.ToInt32(Math.Round(9.99))
+
+
+
+
+
+            /*
+            Console.WriteLine("What is your name ?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Length of your name : "+name.Length);
+            Console.WriteLine("Convert to Upper Case : "+name.ToUpper());
+            Console.WriteLine("convert to Lower Case : " + name.ToLower());
+
+
+            string firstName = "chinthaka";
+            string lastName = "senarathna";
+            //Console.WriteLine("your name is "+firstName+lastName);
+            Console.WriteLine("your name is " + string.Concat(firstName, " ",lastName));
+            Console.WriteLine($"your name is {firstName} {lastName}");
+            Console.WriteLine($"your first name is {firstName}"+"\n"+$"your last name is {lastName}");
+            
+
+        
+            Console.WriteLine("What is your name ?");
+            string name = Console.ReadLine();
+
+            for(int i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+
+            Console.WriteLine(name.IndexOf("n"));
+            
+
+            string name = "chinthaka senarathna";
+            Console.WriteLine(name.Substring(charPos));
+            */
+
+
+
+
+
+
+            /*
+            Console.WriteLine("Hi guys, \"Ahhh\" fuck");
+            Console.WriteLine("Your marks \'12\' okay");
+            Console.WriteLine("This \\ is back slash");
+            */
+
+
+
+
+
+
+
+            /*
+            Console.Write("Input your age : ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            //if (age >= 18)
+            //    Console.WriteLine("Okay, You can see");
+
+            //if (age >= 18)
+            //    Console.WriteLine("Okay, You can see");
+            //else
+            //    Console.WriteLine("No, You can\'t see");
+
+            //if (age >= 50)
+            //    Console.WriteLine("you are old");
+            //else if (age >= 30)
+            //    Console.WriteLine("you are middle");
+            //else if (age >= 20)
+            //    Console.WriteLine("you are young");
+            //else
+            //    Console.WriteLine("you are child");
+
+
+            //if (age > 0)
+            //{
+            //    if (age >= 50)
+            //        Console.WriteLine("you are old");
+            //    else if (age >= 30)
+            //        Console.WriteLine("you are middle");
+            //    else if (age >= 20)
+            //        Console.WriteLine("you are young");
+            //    else
+            //        Console.WriteLine("you are child");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid age");
+            //}
+
+
+            //Console.WriteLine(age > 0 ? "valid" : "Invalid");
+            */
+
+
+
+
+
+
+            /*
+            int day = -3;
+
+            if ((day > 0) && (day < 8))
+            {
+                switch (day)
+                {
+                    case 1:
+                        Console.WriteLine("today is a Monday");
+                        break;
+                    case 2:
+                        Console.WriteLine("today is a tuesday");
+                        break;
+                    case 3:
+                        Console.WriteLine("today is a wensday");
+                        break;
+                    case 4:
+                        Console.WriteLine("toay is a thursday");
+                        break;
+                    case 5:
+                        Console.WriteLine("today is a friday");
+                        break;
+                    case 6:
+                        Console.WriteLine("today is a saturday");
+                        break; ;
+                    case 7:
+                        Console.WriteLine("today is a sunday");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid day");
+            }
+            */
+
+
+
+
+
+            /*
+            int i = 0;
+            while (i<10)
+            {
+                Console.WriteLine(i);
+                i++;
+                Console.WriteLine(i);
+            }
+            */
+
+
+
+
+            /*
+            int i = 0;
+            for (; i < 10;)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            */
+
+
+
+            /*
+            int[] numbers = { 1, 2, 3, 4, 5 };
+            foreach(int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+            */
+
+
+
+
+
+
+            /*
+            for (int i = 0; i < 5; i++)
+            {
+                if (i == 2)
+                    continue;
+
+                if (i == 3)
+                    break;
+                Console.WriteLine(i);
+            }
+            */
+
+
+
+
+            /*
+            int[] array = { 1, 2, 3, 4, 5};
+            
+            int[] array_ = new int[5];
+            int[] array__;
+            array__ = new int[5];
+            array__ = new int[5] { 1, 2, 3, 4, 4};
+            int[] array___;
+            array___ = new int[] { 1, 2, 3, 4, 5, 6 };
+
+
+
+            int[,] numbers = { { 1, 2, 3 }, { 1, 2, 3 } };
+            int[,] numbers_ = new int[2, 3];
+
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+            numbers[0, 1] = 0;
+
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine(numbers.Length);
+            Console.WriteLine(numbers.GetLength(0));
+
+            Array.Sort(array);
+            */
+
+
+
+
+            long l = 123L;
+            int i = (int)l;
+            int i_ = Convert.ToInt32(l);
+        }
+    }
+}
